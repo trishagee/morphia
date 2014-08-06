8d8ecea37fdba3a90d9cb2853196510af9173477
Trisha Gee <trishagee@github.com>
2014-08-06 11:47:52 +0200

    [Geo API #643] Added support for most basic GeoJSON LineString type (supports saving entities only).

--- morphia/src/main/java/org/mongodb/morphia/query/GeoJson.java
+++ morphia/src/main/java/org/mongodb/morphia/query/GeoJson.java
@@ -15,10 +15,8 @@ public final class GeoJson {
     }
 
     /**
-     * Create a new Point representing a 
-     * <a href="http://docs.mongodb.org/manual/app
-     * s/geospatial-indexes/#geojson-objects">GeoJSON</a>
-     * point type.  Supported by server versions 2.4 and above.
+     * Create a new Point representing a <a href="http://docs.mongodb.org/manual/apps/geospatial-indexes/#geojson-objects">GeoJSON</a> point
+     * type.  Supported by server versions 2.4 and above.
      *
      * @param latitude  the point's latitude coordinate
      * @param longitude the point's longitude coordinate
@@ -28,23 +26,34 @@ public final class GeoJson {
         return new Point(latitude, longitude);
     }
 
+    /**
+     * Create a new LineString representing a <a href="http://docs.mongodb.org/manual/apps/geospatial-indexes/#geojson-objects">GeoJSON</a>
+     * LineString type.  Supported by server versions 2.4 an above.
+     *
+     * @param points an ordered series of Points that make up the line
+     * @return a LineString instance representing a series of ordered points that make up a line
+     */
+    public static LineString lineString(final Point... points) {
+        return new LineString(points);
+    }
+
     public static class Point {
         private final String type = "Point";
         private double[] coordinates;
-    
+
         //needed for Morphia serialisation
         @SuppressWarnings("unused")
-        public Point() {
+        private Point() {
         }
-    
-        public Point(final double latitude, final double longitude) {
+
+        Point(final double latitude, final double longitude) {
             this.coordinates = new double[]{longitude, latitude};
         }
-    
+
         DBObject asDBObject() {
             return new BasicDBObject("type", type).append("coordinates", coordinates);
         }
-    
+
         @Override
         public boolean equals(final Object o) {
             if (this == o) {
@@ -53,32 +62,44 @@ public final class GeoJson {
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
-    
+
             Point point = (Point) o;
-    
+
             if (!Arrays.equals(coordinates, point.coordinates)) {
                 return false;
             }
             if (!type.equals(point.type)) {
                 return false;
             }
-    
+
             return true;
         }
-    
+
         @Override
         public int hashCode() {
             int result = type.hashCode();
             result = 31 * result + Arrays.hashCode(coordinates);
             return result;
         }
-    
+
         @Override
         public String toString() {
             return "Point{"
                    + "type='" + type + '\''
                    + ", coordinates=" + Arrays.toString(coordinates)
                    + '}';
+        }
+    }
+
+    public static class LineString {
+        private final String type = "LineString";
+        private final double[][] coordinates;
+
+        LineString(final Point... points) {
+            this.coordinates = new double[points.length][2];
+            for (int i = 0; i < points.length; i++) {
+                this.coordinates[i] = points[i].coordinates;
+            }
         }
     }
 }
--- morphia/src/test/java/org/mongodb/morphia/geo/SaveGeoEntitiesTest.java
+++ morphia/src/test/java/org/mongodb/morphia/geo/SaveGeoEntitiesTest.java
@@ -4,18 +4,23 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.junit.Test;
 import org.mongodb.morphia.TestBase;
+import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.query.GeoJson;
 import org.mongodb.morphia.testutil.JSONMatcher;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mongodb.morphia.query.GeoJson.point;
 
+/**
+ * Test driving features for Issue 643 - add support for saving entities with GeoJSON.
+ */
 public class SaveGeoEntitiesTest extends TestBase {
     @Test
     public void shouldSaveAnEntityWithALocationStoredAsAPoint() {
         // given
-        City city = new City("New City", GeoJson.point(3.0, 7.0));
+        City city = new City("New City", point(3.0, 7.0));
 
         // when
         getDs().save(city);
@@ -32,7 +37,42 @@ public class SaveGeoEntitiesTest extends TestBase {
                                                                 + "  coordinates: [7.0, 3.0]"
                                                                 + " }" 
                                                                 + "}"));
-
     }
 
+    @Test
+    public void shouldSaveAnEntityWithALineStringGeoJsonType() {
+        // given
+        Route route = new Route("My Route", GeoJson.lineString(point(1, 2), point(3, 5), point(19, 13)));
+
+        // when
+        getDs().save(route);
+
+        // then use the underlying driver to ensure it was persisted correctly to the database
+        DBObject storedRoute = getDs().getCollection(Route.class).findOne(new BasicDBObject("name", "My Route"),
+                                                                        new BasicDBObject("_id", 0).append("className", 0));
+        assertThat(storedRoute, is(notNullValue()));
+        // lat/long is always long/lat on the server
+        assertThat(storedRoute.toString(), JSONMatcher.jsonEqual("  {"
+                                                                + " name: 'My Route',"
+                                                                + " route:  "
+                                                                + " {"
+                                                                + "  type: 'LineString', "
+                                                                + "  coordinates: [ [ 2.0,  1.0]," 
+                                                                + "                 [ 5.0,  3.0],"
+                                                                + "                 [13.0, 19.0] ]"
+                                                                + " }"
+                                                                + "}"));
+    }
+
+    @Entity
+    @SuppressWarnings("UnusedDeclaration")
+    private static class Route {
+        private final String name;
+        private final GeoJson.LineString route;
+
+        public Route(final String name, final GeoJson.LineString route) {
+            this.name = name;
+            this.route = route;
+        }
+    }
 }
